f6f625370899d0a2f4b86dc4b958c411fef7249c
Firewolf1998 <101706787+Firewolf1998@users.noreply.github.com>
2022-11-16 13:36:03 +0100

    Update tipcalculator.py

--- Day_2/tipcalculator.py
+++ Day_2/tipcalculator.py
@@ -13,5 +13,6 @@ percent = int(input("What percentage tip would you like to give? 10, 12 or 15? "
 people = int(input("How many people to split the bill? "))
 split_bill = bill/people
 final_amount = round(split_bill*(1+0.01*percent),2)
+#final_amount = "{:.2f}".format(split_bill*(1+0.01*percent))
 
 print(f"Each person should pay: {final_amount}")
