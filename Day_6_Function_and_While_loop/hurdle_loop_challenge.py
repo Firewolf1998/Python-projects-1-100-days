a25770fbab1ed3400194fa5bcf56d071591fb46b
Firewolf1998 <afirewolf98@gmail.com>
2022-11-26 12:52:59 +0100

    Created hurdle_loop_challenge.py

---
+++ Day_6_Function_and_While_loop/hurdle_loop_challenge.py
@@ -0,0 +1,21 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def race():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+for step in range(0,6):
+    race()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
