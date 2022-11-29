68ef7f8e50ad1f68ae4ae96c6ed7c72d4f02d490
Johnal Dsouza <101706787+Firewolf1998@users.noreply.github.com>
2022-11-29 10:44:52 +0100

    Create hangman3.py

---
+++ Day_7_Hangman_game/hangman3.py
@@ -0,0 +1,33 @@
+'''
+Checks if there are no "_" in display list and outputs you have won
+'''
+#Step 3
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+tries = True
+while tries:
+  guess = input("Guess a letter: ").lower()
+  
+#Check guessed letter
+  for position in range(word_length):
+    letter = chosen_word[position]
+    #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+    if letter == guess:
+      display[position] = letter
+  if "_" not in display:
+    tries = False
+    print("You have won")
+  print(display)
