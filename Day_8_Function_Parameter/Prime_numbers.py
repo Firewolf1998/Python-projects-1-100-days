9708eb6dc212e349df16d264a1a1df3401cd59a7
Johnal Dsouza <101706787+Firewolf1998@users.noreply.github.com>
2022-11-30 11:01:18 +0100

    Create Prime_numbers.py

---
+++ Day_8_Function_Parameter/Prime_numbers.py
@@ -0,0 +1,23 @@
+"""
+You need to write a function that checks whether if the number passed into it is a prime number or not.
+"""
+
+#Write your code below this line 👇
+def prime_checker(number):
+    prime = True
+    for i in range(2,51):
+        if number%i==0 and i != number:
+            prime = False
+    if prime == False:
+        print("It's not a prime number.")
+    else:
+        print("It's a prime number.")
+
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
