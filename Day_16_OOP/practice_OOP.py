afb3bfc39e3abde5d7805a91b9f5ae12af0e408f
Firewolf1998 <afirewolf98@gmail.com>
2022-12-21 16:55:00 +0100

    created practice_OOP.py

--- Day_16_OOP/practice_OOP.py
+++ Day_16_OOP/practice_OOP.py
@@ -1,3 +1,13 @@
 from prettytable import PrettyTable
+
 table = PrettyTable()
+
+# changing the method
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Carmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+# Changing the attribute
+print(table.align)
+table.align = "l"
+
 print(table)
